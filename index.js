248ba26b2d6be87ef11f198efbb73f306f9b4f3d
Ayesha <ayesha18mbstu@gmail.com>
2023-07-24 21:51:41 +0600

    fatching college by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,10 +30,16 @@ async function run() {
          res.send(result);
     })
 
+    app.get("/colleges/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await collegeCollection.findOne(query);
+      res.send(result);
+  })
 
 
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
